565235cb7c35662cb3fd00da514b30bf87500adb
TanjonaCod <rafaltanjonaniaina@gmail.com>
2025-01-18 09:58:00 +0300

    indentqtion

--- Js/main.js
+++ Js/main.js
@@ -15,18 +15,18 @@ api().then(e =>{
     let htmlStr = '';
 
     e.forEach((el,i) => {
-        htmlStr+=
+        htmlStr +=
         `
-    <div class="col-md-4">
-        <div class="card w-100">
-          <img src="${el.image}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${el.title.slice(0,25)}</h5>
-            <p class="card-text">${el.title.slice(0,50)+' ...'}</p>
-            <button class="btn btn-primary">voir plus</button>
-          </div>
-        </div>
-    </div>
+            <div class="col-md-4">
+                <div class="card w-100">
+                <img src="${el.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${el.title.slice(0,25)}</h5>
+                    <p class="card-text">${el.title.slice(0,50)+' ...'}</p>
+                    <button class="btn btn-primary">voir plus</button>
+                </div>
+                </div>
+            </div>
 
         `
     });
